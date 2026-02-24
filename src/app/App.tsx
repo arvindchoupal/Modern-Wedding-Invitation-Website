5c86e9e50b60af90f0e681db73fbce7cfb041841
arvindchoupal <arvindchoupal1999@gmail.com>
2026-02-24 19:40:38 +0530

    some changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,18 @@
-import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import ReactGA from "react-ga4";
 import Arvind from "./arvind";
 import Ajit from "./ajir";
-
-// const TRACKING_ID = "G-KSGJBCBQ00";
-// ReactGA.initialize(TRACKING_ID);
 
 export default function App() {
   const location = useLocation();
 
-//   useEffect(() => {
-//     ReactGA.send({
-//       hitType: "pageview",
-//       page: location.pathname + location.search,
-//     });
-//   }, [location]);
+  const isAjit = location.pathname.includes("Ajit_Sanjana");
 
   return (
     <Routes>
-      <Route path="/" element={<Arvind />} />
-      <Route path="/Ajit_Sanjana" element={<Ajit />} />
+      <Route
+        path="*"
+        element={isAjit ? <Ajit /> : <Arvind />}
+      />
     </Routes>
   );
 }
